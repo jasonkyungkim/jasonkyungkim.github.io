8e8ffa53228eb41cba384bfbbbf49c790c8c9065
ckim5 <ckim5@swarthmore.edu>
2021-02-14 14:28:40 -0500

    clear up js landing page

--- js/cover.js
+++ js/cover.js
@@ -9,12 +9,6 @@ anime.timeline({loop: false})
     duration: 750,
     delay: (el, i) => 70 * i
   }).add({
-    targets: '.ml6',
-    opacity: 1,
-    // duration: 1000,
-    // easing: "easeOutExpo",
-    // delay: 1000
-  }).add({
     targets: '.ml15 .word',
     scale: [2,1],
     opacity: [0,1],
